df244405139c057e13d4ff5fdaf9407f34bc3ef2
Exile <exile@chamber.ee>
2016-10-13 19:18:22 +0000

    Working pushbutton example
    - Tested in iOS9 and iOS10
    - Running 80 days

--- index.js
+++ index.js
@@ -1,76 +1,48 @@
-var Service, Characteristic, uuid;
-
-var inherits = require('util').inherits;
+var Service, Characteristic;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  uuid = homebridge.hap.uuid;
-
-  fixInheritance(PBPlatform.PushButton, Characteristic);
-  fixInheritance(PBPlatform.PushButtonService, Service);
-
   homebridge.registerAccessory("homebridge-pushbutton", "PushButton", PushButtonAccessory);
-  homebridge.registerPlatform("homebridge-pushbutton", "PushButton", PBPlatform);
-}
-
-function fixInheritance(subclass, superclass) {
-    var proto = subclass.prototype;
-    inherits(subclass, superclass);
-    subclass.prototype.parent = superclass.prototype;
-    for (var mn in proto) {
-        subclass.prototype[mn] = proto[mn];
-    }
-}
-
-function PBPlatform(log, config) {
-  this.log = log;
-  this.config = config;
 }
 
 function PushButtonAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
-  this.buttonName = config["button_name"] || this.name;
+  this.buttonName = config["button_name"] || this.name; // fallback to "name" if you didn't specify an exact "button_name"
+  this.binaryState = 0; // switch state, default is OFF
   this.log("Starting a PushButton device with name '" + this.buttonName + "'...");
+  this.pushButtonService;
+  this.timeout = 2; // Timeout in seconds
 }
 
-PushButtonAccessory.prototype.Activate = function(activation, callback) {
-  this.log("Received signal on '%s' = %s", this.buttonName, activation);
+PushButtonAccessory.prototype.getPowerOn = function(callback) {
+  var powerOn = this.binaryState > 0;
+  this.log("Power state for the '%s' is %s", this.buttonName, this.binaryState);
+  callback(null, powerOn);
+}
+
+PushButtonAccessory.prototype.setPowerOn = function(powerOn, callback) {
+  var self = this;
+  this.binaryState = powerOn ? 1 : 0; // wemo langauge
+  this.log("Set power state on the '%s' to %s", this.buttonName, this.binaryState);
   callback(null);
+
+  if(powerOn) {
+    setTimeout(function() {
+      self.log("BEEP! BOOP!");
+      self.pushButtonService.getCharacteristic(Characteristic.On).setValue(0);
+    }, this.timeout * 1000);
+  }
 }
 
 PushButtonAccessory.prototype.getServices = function() {
-    var pushButtonService = new PBPlatform.PushButtonService(this.name);
+    this.pushButtonService = new Service.Switch(this.name);
 
-    pushButtonService
-      .getCharacteristic(PBPlatform.PushButton)
-      .on('set', this.Activate.bind(this));
+    this.pushButtonService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getPowerOn.bind(this))
+      .on('set', this.setPowerOn.bind(this));
 
-    return [pushButtonService];
+    return [this.pushButtonService];
 }
-
-//
-
-PBPlatform.PushButton = function() {
-  var charUUID = uuid.generate('PBPlatform:customchar:PushButton');
-  Characteristic.call(this, 'Activate', charUUID);
-  this.setProps({
-    format: Characteristic.Formats.BOOL,
-    perms: [Characteristic.Perms.WRITE]
-  });
-  this.value = this.getDefaultValue();
-};
-
-PBPlatform.PushButtonService = function(displayName, subtype) {
-  var serviceUUID = uuid.generate('PBPlatform:customservice:PushButtonService')
-  Service.call(this, displayName, serviceUUID, subtype);
-
-  // Required Characteristics
-  this.addCharacteristic(PBPlatform.PushButton);
-
-  // Optional Characteristics
-  this.addOptionalCharacteristic(Characteristic.Name);
-};
-
-//
